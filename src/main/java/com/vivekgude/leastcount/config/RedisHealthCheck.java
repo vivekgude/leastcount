d1a5eb23f456a2ea1f007499c9d21dc2efc5ce22
Vivek Gude <gudevivek@gmail.com>
2025-05-25 03:49:16 +0530

    added redis health check

---
+++ src/main/java/com/vivekgude/leastcount/config/RedisHealthCheck.java
@@ -0,0 +1,33 @@
+package com.vivekgude.leastcount.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import redis.clients.jedis.JedisPool;
+
+@Component
+@Slf4j
+public class RedisHealthCheck implements CommandLineRunner {
+
+    private final JedisPool jedisPool;
+
+    public RedisHealthCheck(JedisPool jedisPool) {
+        this.jedisPool = jedisPool;
+    }
+
+    @Override
+    public void run(String... args) {
+        try (var jedis = jedisPool.getResource()) {
+            String pong = jedis.ping();
+            if ("PONG".equals(pong)) {
+                log.info("Successfully connected to Redis server");
+            } else {
+                log.error("Failed to connect to Redis server");
+                throw new RuntimeException("Redis connection test failed");
+            }
+        } catch (Exception e) {
+            log.error("Error connecting to Redis server - {}", e.getMessage());
+            throw new RuntimeException("Failed to connect to Redis server", e);
+        }
+    }
+} 
